b48777f86bd0e71c90f19bb565d0370b09defabf
Matt Werner <mrwerner392@gmail.com>
2019-07-06 14:32:26 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-
+# method that has required and default arguments, outputs strings and returns a value
 def meal_choice(side1, side2, protein = "meat")
   meal = "A plate of #{protein} with #{side1} and #{side2}."
   puts "What a nutritious meal!"
