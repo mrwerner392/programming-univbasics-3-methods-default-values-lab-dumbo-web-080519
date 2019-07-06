ae5a64a9e1cd321abf1881d56548f93bd6b5ba45
Matt Werner <mrwerner392@gmail.com>
2019-07-06 14:32:41 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
-# Your code here
+# method that has required and default arguments, outputs strings and returns a value
+def meal_choice(side1, side2, protein = "meat")
+  meal = "A plate of #{protein} with #{side1} and #{side2}."
+  puts "What a nutritious meal!"
+  puts "#{meal}"
+  return meal
+end
